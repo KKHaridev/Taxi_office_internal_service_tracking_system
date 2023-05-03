95d083e015a9b8c21a636a05deb67a34f212b2ad
K K Haridev <kkharidev@gmail.com>
2023-05-03 15:09:31 +0530

    Wrapped App with in ChakraProvider and updated theme and fonts

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/poppins/400.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const theme = extendTheme({
+  colors: {
+    brand: {
+      purple: "#6E2594",
+      yellow: "#ECD444",
+      offwhite: "#FCFBF9",
+    },
+  },
+  fonts: {
+    body: `'Poppins', sans-serif`,
+    heading: `'Poppins', sans-serif`,
+    button: `'Poppins', sans-serif`,
+
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
