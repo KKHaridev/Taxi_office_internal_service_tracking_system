70ad1eb481e2f2cb7ff3a98002c6b53e693ab06b
K K Haridev <kkharidev@gmail.com>
2023-05-03 21:26:58 +0530

    Created new route to dashboard and updated login page route

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Login } from "@driver/Login";
 import { Dashboard } from "@driver/Dashboard";
+import { Layout } from "@components";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Login />}></Route>
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </>
