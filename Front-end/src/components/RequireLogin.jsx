305901740d1788f586851983ce338f21b53caba0
K K Haridev <kkharidev@gmail.com>
2023-05-04 14:51:43 +0530

    Created new component to check auth requirement

---
+++ Front-end/src/components/RequireLogin.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export const RequireLogin = ({ children }) => {
+  const auth = useAuth();
+  if (!auth.user) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
