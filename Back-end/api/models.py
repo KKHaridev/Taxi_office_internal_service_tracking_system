8aec17bb65b9640a5a634a6f2bd53c07c6c8dfdb
CYRIL C KURIAN <cyril.kurian2@gmail.com>
2023-05-06 21:30:08 +0530

    Updated Driver model

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -1,14 +1,31 @@
 from django.db import models
+import string
+import random
 
+def generate_unique_code():
+    length = 6
+
+    while True:
+        driver_id = ''.join(random.choices(string.ascii_uppercase, k=length))
+        if Driver.objects.filter(driver_id=driver_id).count() == 0:
+            break
+
+    return driver_id
 # Create your models here.
 class Driver(models.Model):
-    driver_id = models.CharField(max_length=10,default="",unique=True)
+    driver_id = models.CharField(max_length=10,default=generate_unique_code,unique=True)
     driver_name = models.CharField(max_length=30,default="",unique=False)
     driver_email =models.CharField(max_length=50)
     taxi_num = models.CharField(max_length=10,unique=True)
     driver_phone = models.CharField(max_length=11)
-    driver_test_date = models.DateField(auto_now=False, auto_now_add=False,max_length=12) 
-    driver_pollution_validity = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
-    driver_insurance = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
+    taxi_test_date = models.DateField(auto_now=False, auto_now_add=False,max_length=12) 
+    taxi_pollution_validity = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
+    taxi_insurance = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
+    taxi_type = models.CharField(max_length=10)
+    taxi_model= models.CharField(max_length=10)
+    driver_status = models.CharField(max_length=10, default="available")
+    driver_upi = models.CharField(max_length=10, unique=True)
+
+    
     
     
