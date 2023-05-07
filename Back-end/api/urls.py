45c0251121a02455bb5d4934adef7a8349935435
shareehshakkir <shareehshakkir@gmail.com>
2023-05-07 20:06:26 +0530

    Create views and urls for received requests

--- Back-end/api/urls.py
+++ Back-end/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import include, path
-from .views import DriverView
+from .views import DriverView, ReceivedView
 
 urlpatterns = [
-    path('home',DriverView.as_view()),
+    path('home', DriverView.as_view()),
+    path('recreq', ReceivedView.as_view()),
+
 ]
--- Back-end/api/views.py
+++ Back-end/api/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from rest_framework import generics, status
-from .serializers import DriverSerializer, CreateDriverSerializer
-from .models import Driver
+from .serializers import DriverSerializer, CreateDriverSerializer, ReceivedSerializer
+from .models import Driver, ReceivedTrip
 from rest_framework.views import APIView
-from rest_framework.response import Response 
+from rest_framework.response import Response
 
 # Create your views here.
+
 
 class DriverView(generics.ListAPIView):
     queryset = Driver.objects.all()
     serializer_class = DriverSerializer
+
 
 class CreateDriverView(APIView):
     serializer_class = CreateDriverSerializer
@@ -17,10 +19,30 @@ class CreateDriverView(APIView):
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
             self.request.session.create()
-        
+
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             driver_name = serializer.data.driver_name
             driver_email = serializer.data.driver_email
             taxi_num = serializer.data.taxi_num
             queryset = Driver.objects.filter(driver_id=driver_id)
+
+
+class ReceivedView(generics.CreateAPIView):
+    queryset = ReceivedTrip.objects.all()
+    serializer_class = ReceivedSerializer
+
+
+class CreateNewReqView(APIView):
+    serializer_class = ReceivedSerializer
+
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user_name = serializer.data.user_name
+            start_from = serializer.data.start_from
+            destination = serializer.data.destination
+            status = serializer.data.status
