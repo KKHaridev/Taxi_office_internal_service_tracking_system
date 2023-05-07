4d8191d2e41edf090bb64e211d00ee1e6b1c87bb
shareehshakkir <shareehshakkir@gmail.com>
2023-05-07 07:57:35 +0530

    Created ReceivedTrip model

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 import string
 import random
 
+
 def generate_unique_code():
     length = 6
 
@@ -11,21 +12,39 @@ def generate_unique_code():
             break
 
     return driver_id
+
+
 # Create your models here.
 class Driver(models.Model):
-    driver_id = models.CharField(max_length=10,default=generate_unique_code,unique=True)
-    driver_name = models.CharField(max_length=30,default="",unique=False)
-    driver_email =models.CharField(max_length=50)
-    taxi_num = models.CharField(max_length=10,unique=True)
+    driver_id = models.CharField(
+        max_length=10, default=generate_unique_code, unique=True)
+    driver_name = models.CharField(max_length=30, default="", unique=False)
+    driver_email = models.CharField(max_length=50)
+    taxi_num = models.CharField(max_length=10, unique=True)
     driver_phone = models.CharField(max_length=11)
+<<<<<<< Updated upstream
     taxi_test_date = models.DateField(auto_now=False, auto_now_add=False,max_length=12) 
     taxi_pollution_validity = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
     taxi_insurance = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
     taxi_type = models.CharField(max_length=10,default='')
     taxi_model= models.CharField(max_length=10,default='')
+=======
+    taxi_test_date = models.DateField(
+        auto_now=False, auto_now_add=False, max_length=12)
+    taxi_pollution_validity = models.DateField(
+        auto_now=False, auto_now_add=False, max_length=12)
+    taxi_insurance = models.DateField(
+        auto_now=False, auto_now_add=False, max_length=12)
+    taxi_type = models.CharField(max_length=10)
+    taxi_model = models.CharField(max_length=10)
+>>>>>>> Stashed changes
     driver_status = models.CharField(max_length=10, default="available")
     driver_upi = models.CharField(max_length=10, default='', unique=True)
 
-    
-    
-    
+
+class ReceivedTrip(models.Model):
+    user_name = models.CharField(max_length=250)
+    start_from = models.CharField(max_length=250)
+    destination = models.CharField(max_length=250)
+    requested_time = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20)
--- Back-end/api/serializers.py
+++ Back-end/api/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Driver
+from .models import ReceivedTrip
+
 
 class DriverSerializer(serializers.ModelSerializer):
     class Meta:
+<<<<<<< Updated upstream
             model = Driver
             fields = ('driver_id','driver_name','driver_email','taxi_num','driver_phone','driver_test_date','driver_pollution_validity','driver_insurance')
 
@@ -10,3 +13,14 @@ class CreateDriverSerializer(serializers.ModelSerializer):
       class Meta:
             model = Driver
             fields = ('driver_name','driver_email','taxi_num')
+=======
+        model = Driver
+        fields = ('driver_id', 'driver_name', 'driver_email', 'taxi_num', 'driver_phone', 'driver_test_date',
+                  'driver_pollution_validity', 'driver_insurance')
+
+
+class ReceivedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReceivedTrip
+        fields = ('user_name', 'start_from', 'destination', 'requested_time')
+>>>>>>> Stashed changes
