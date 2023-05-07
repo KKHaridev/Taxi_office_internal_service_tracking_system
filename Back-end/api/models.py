46b4e25d8d571ba0029d53e6ac7141aed3c5edab
CYRIL C KURIAN <cyril.kurian2@gmail.com>
2023-05-07 08:50:18 +0530

    correction in serializers.py

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -16,28 +16,16 @@ def generate_unique_code():
 
 # Create your models here.
 class Driver(models.Model):
-    driver_id = models.CharField(
-        max_length=10, default=generate_unique_code, unique=True)
+    driver_id = models.CharField(max_length=10, default=generate_unique_code, unique=True)
     driver_name = models.CharField(max_length=30, default="", unique=False)
     driver_email = models.CharField(max_length=50)
     taxi_num = models.CharField(max_length=10, unique=True)
     driver_phone = models.CharField(max_length=11)
-<<<<<<< Updated upstream
     taxi_test_date = models.DateField(auto_now=False, auto_now_add=False,max_length=12) 
     taxi_pollution_validity = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
     taxi_insurance = models.DateField(auto_now=False, auto_now_add=False,max_length=12)
     taxi_type = models.CharField(max_length=10,default='')
     taxi_model= models.CharField(max_length=10,default='')
-=======
-    taxi_test_date = models.DateField(
-        auto_now=False, auto_now_add=False, max_length=12)
-    taxi_pollution_validity = models.DateField(
-        auto_now=False, auto_now_add=False, max_length=12)
-    taxi_insurance = models.DateField(
-        auto_now=False, auto_now_add=False, max_length=12)
-    taxi_type = models.CharField(max_length=10)
-    taxi_model = models.CharField(max_length=10)
->>>>>>> Stashed changes
     driver_status = models.CharField(max_length=10, default="available")
     driver_upi = models.CharField(max_length=10, default='', unique=True)
 
--- Back-end/api/serializers.py
+++ Back-end/api/serializers.py
@@ -5,22 +5,15 @@ from .models import ReceivedTrip
 
 class DriverSerializer(serializers.ModelSerializer):
     class Meta:
-<<<<<<< Updated upstream
             model = Driver
-            fields = ('driver_id','driver_name','driver_email','taxi_num','driver_phone','driver_test_date','driver_pollution_validity','driver_insurance')
+            fields = ('driver_id','driver_name','driver_email','taxi_num','driver_phone','taxi_test_date','taxi_pollution_validity','taxi_insurance','taxi_type','taxi_model','driver_status','driver_upi')
 
 class CreateDriverSerializer(serializers.ModelSerializer):
       class Meta:
             model = Driver
             fields = ('driver_name','driver_email','taxi_num')
-=======
-        model = Driver
-        fields = ('driver_id', 'driver_name', 'driver_email', 'taxi_num', 'driver_phone', 'driver_test_date',
-                  'driver_pollution_validity', 'driver_insurance')
-
 
 class ReceivedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ReceivedTrip
-        fields = ('user_name', 'start_from', 'destination', 'requested_time')
->>>>>>> Stashed changes
+     class Meta:
+            model = ReceivedTrip
+            fields = ('user_name', 'start_from', 'destination','requested_time', 'status')
