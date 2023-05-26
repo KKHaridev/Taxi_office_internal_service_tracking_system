96be0b4122d0ddc9d166907afd7b43351f82db44
K K Haridev <kkharidev@gmail.com>
2023-05-26 13:35:27 +0530

    Updated cards in dashboard

--- Front-end/src/components/Card/Card.jsx
+++ Front-end/src/components/Card/Card.jsx
@@ -1,25 +1,46 @@
 import React from "react";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Flex, Heading, Text, Icon, Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-export const Card = ({ title, number }) => {
+export const Card = ({ icon, title, number }) => {
   return (
-    <Link to={title.toLowerCase()}>
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      flexDir="column"
+      gap="15px"
+      w="275px"
+      height={["225px", "275px"]}
+      bg="white"
+      borderRadius="7px"
+      boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+    >
       <Flex
+        w={50}
+        h={50}
+        borderRadius="50%"
+        bg="brand.purple"
         alignItems="center"
         justifyContent="center"
-        flexDir="column"
-        gap="15px"
-        minW="300px"
-        height={["100px", "125px"]}
-        bg="white"
-        borderRadius="7px"
       >
-        <Heading as="h4" size="md">
-          {title}
-        </Heading>
-          
-        <Text dangerouslySetInnerHTML={{ __html: number }}></Text>
+        <Icon as={icon} w={30} h={30} color="white" />
       </Flex>
-    </Link>
+
+      <Heading as="h3" size="sm">
+        {title}
+      </Heading>
+
+      <Text dangerouslySetInnerHTML={{ __html: number }} fontSize="35px" fontWeight="bolder"></Text>
+      <Link to={title.toLowerCase()}>
+        <Button
+          h="35px"
+          w="140px"
+          bg="brand.purple"
+          _hover={{ bg: "purple.700" }}
+          color="white"
+        >
+          View
+        </Button>
+      </Link>
+    </Flex>
   );
 };
