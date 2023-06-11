43ab99380de01b5e7400ba994198b4886d6625ae
K K Haridev <kkharidev@gmail.com>
2023-06-11 22:47:11 +0530

    Updated navigation in RequireLogin component

--- Front-end/src/components/RequireLogin.jsx
+++ Front-end/src/components/RequireLogin.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const RequireLogin = ({ children }) => {
   const auth = useAuth();
+  const path = useLocation();
+  const admin = path.pathname.includes("admin");
   if (!auth.user) {
-    return <Navigate to="/login" replace={true} />;
+    return <Navigate to={admin ? "/admin/login" : "/login"} replace={true} />;
   }
   return children;
 };
