59fab8bd0ee4a14c42de0a91726ece882bf74866
K K Haridev <kkharidev@gmail.com>
2023-06-21 23:48:51 +0530

    Updated BreadCrumb navigation

--- Front-end/src/components/Breadcrumb.jsx
+++ Front-end/src/components/Breadcrumb.jsx
@@ -7,6 +7,7 @@ import {
 import { ChevronRightIcon } from "@chakra-ui/icons";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Breadcrumb = () => {
   const navigate = useNavigate();
@@ -19,20 +20,22 @@ export const Breadcrumb = () => {
   return (
     <BreadCrumb spacing="8px" separator={<ChevronRightIcon color="gray.500" />}>
       <BreadcrumbItem isCurrentPage={location.pathname == "/"}>
-        <BreadcrumbLink href={admin ? "/admin" : "/"}>Home</BreadcrumbLink>
+        <BreadcrumbLink as={Link} href={admin ? "/admin" : "/"}>Home</BreadcrumbLink>
       </BreadcrumbItem>
       {locations.map((location, index) => (
         <BreadcrumbItem
           key={index}
           isCurrentPage={index === locations.length - 1}
         >
-          <BreadcrumbLink
+          <BreadcrumbLink color="brand.purple"
             onClick={() => {
-              navigate(-1);
+              index === locations.length - 1
+                ? ""
+                : navigate(admin?`/admin${locations[locations.length-2]}`:`/${locations[locations.length-2]}`);
             }}
             textTransform="capitalize"
           >
-            {location.replace("_", " ")}
+            {location.replaceAll("_", " ")}
           </BreadcrumbLink>
         </BreadcrumbItem>
       ))}
