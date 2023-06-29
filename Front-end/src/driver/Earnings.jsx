2c29cc42502d7a944c2e62af403c5f0461d810c8
K K Haridev <kkharidev@gmail.com>
2023-06-29 21:14:54 +0530

    Updated earings page with user data from backend

--- Front-end/src/driver/Earnings.jsx
+++ Front-end/src/driver/Earnings.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import { Breadcrumb } from "@components/Breadcrumb";
 import { Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import { useAuth } from "@context/AuthContext";
-import { useData1 } from "../hooks/useData";
+import { useData } from "@hooks/useData";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Row = ({ heading, count, price }) => {
   return (
@@ -19,48 +23,62 @@ export const Earnings = () => {
   const { user } = useAuth();
   const id = user?.driver_id;
 
-  const { isLoading, error, data } = useData1(
-    "earnings",
-    `api/${user?.driver_id}/earnings`
-  );
+  const { isLoading, error, data } = useData("earnings", `api/earnings`);
   if (isLoading) {
     return <h1>Loading</h1>;
   }
-  console.log(data);
+  const { driver_id, driver_name, total_rides, total_earnings, ...details } =
+    data;
 
+  const chartData = {
+    labels: Object.keys(details).map((item, key) =>
+      item.replace("_", " ").toUpperCase()
+    ),
+    datasets: [
+      {
+        label: "Rs",
+        data: Object.values(details),
+        backgroundColor: ["#FFCB14", "#19E32D"],
+        borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+        borderWidth: 1,
+      },
+    ],
+  };
   return (
     <>
       <Breadcrumb />
-      <Flex
-        bg="white"
-        w="320px"
-        h="300px"
-        mt="5px"
-        justifyContent="center"
-        alignItems="center"
-        borderRadius="md"
-        flexDir="column"
-        gap="20px"
-        p="15px"
-      >
-        <Heading
-          as="h3"
-          fontSize={["25px"]}
-          fontWeight="light"
-          color="brand.purple"
+      <Flex direction={{base:"column",md:"row"}}>
+        <Flex
+          bg="white"
+          w="320px"
+          h="300px"
+          mt="5px"
+          justifyContent="center"
+          alignItems="center"
+          borderRadius="md"
+          flexDir="column"
+          gap="20px"
+          p="15px"
         >
-          Earnings
-        </Heading>
-        <Flex direction="column" gap="24px" w="90%" alignItems="center">
-          <Row heading="Total Rides" count={data.total_rides} />
+          <Heading
+            as="h3"
+            fontSize={["25px"]}
+            fontWeight="light"
+            color="brand.purple"
+          >
+            Earnings
+          </Heading>
+          <Flex direction="column" gap="24px" w="90%" alignItems="center">
+            <Row heading="Total Rides" count={total_rides} />
+            <Row heading="Total Earnings" count={total_earnings} price={true} />
 
-          {Object.entries(data).map(([heading, count]) =>
-            heading === "total_rides" ? (
-              ""
-            ) : (
+            {Object.entries(details).map(([heading, count]) => (
               <Row heading={heading} count={[count]} price={true} />
-            )
-          )}
+            ))}
+          </Flex>
+        </Flex>
+        <Flex w={300} h={500} marginLeft={{base:"0",md:"30px"}}>
+          <Pie data={chartData} />
         </Flex>
       </Flex>
     </>
