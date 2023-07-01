47acc0f4b0cf86a587b2a1e2fc692623fc75c2dd
K K Haridev <kkharidev@gmail.com>
2023-07-01 10:25:26 +0530

    Updated useData hook

--- Front-end/src/hooks/useData.js
+++ Front-end/src/hooks/useData.js
@@ -1,7 +1,26 @@
 import { useMutation, useQuery } from "react-query";
+import { useAuth } from "../context/AuthContext";
+
+/* const fetchData = (path) => {
+  return fetch(`http://localhost:3000/${path}`).then((res) => res.json());
+};
+
+export const useData = (key, path, options = {}) => {
+  return useQuery(key, () => fetchData(path), {
+    ...options,
+  });
+}; */
+const accessKey = JSON.parse(localStorage.getItem("authTokens"));
+const apiUrl = import.meta.env.VITE_API_URL;
 
 const fetchData = (path) => {
-  return fetch(`http://localhost:3000/${path}`).then((res) => res.json());
+  let headersList = {
+    Authorization: `Bearer ${accessKey.access}`,
+  };
+  return fetch(`${apiUrl}/${path}`, {
+    method: "GET",
+    headers: headersList,
+  }).then((res) => res.json());
 };
 
 export const useData = (key, path, options = {}) => {
@@ -10,16 +29,38 @@ export const useData = (key, path, options = {}) => {
   });
 };
 
-const fetchData1 = (path) => {
-  return fetch(`http://127.0.0.1:8000/${path}`).then((res) => res.json());
-};
+const postReq = async ({ endpoint, data }) => {
+  const res = await fetch(`${apiUrl}/${endpoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
 
-export const useData1 = (key, path, options = {}) => {
-  return useQuery(key, () => fetchData1(path), {
-    ...options,
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
   });
+  let result = await res.json();
+  return result;
 };
 
+const postReqWithAccess = async ({ endpoint, data, accessToken }) => {
+  let headersList = {
+    Authorization: `Bearer ${accessToken}`,
+    "Content-type": "application/json; charset=UTF-8",
+  };
+  const res = await fetch(`${apiUrl}/${endpoint}`, {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: headersList,
+  });
+  const response = await res.json();
+  if (res.statusText == "Bad Request") {
+    let err = "Error Occured.";
+    const error = { err, issues: response };
+    return error;
+  } else {
+    return response;
+  }
+};
 const patchReq = ({ id, data }) => {
   return fetch(`http://localhost:3000/req_rides/${id}`, {
     method: "PATCH",
@@ -58,18 +99,6 @@ const deleteUser = ({ id }) => {
     .then((json) => console.log(json));
 };
 
-const createDriver = async (data) => {
-  return await fetch(`http://localhost:3000/drivers/`, {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-};
-
 export const useAcceptRide = () => {
   return useMutation(patchReq);
 };
@@ -82,6 +111,14 @@ export const useUpdateStatus = () => {
   return useMutation(updateStatus);
 };
 
-export const useCreateDriver = () => {
-  return useMutation(createDriver);
+export const useNewDriver = () => {
+  return useMutation(postReq);
+};
+
+export const useNewDriverAccount = () => {
+  return useMutation(postReqWithAccess, {
+    onError: (error) => {
+      return error;
+    },
+  });
 };
