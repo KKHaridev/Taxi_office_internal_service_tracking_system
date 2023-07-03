91d2fd24f5654cef3f50a88c38187cc97b492781
CYRIL C KURIAN <cyril.kurian2@gmail.com>
2023-07-03 09:00:00 +0530

    changes in model, added dataset model

--- Back-end/api/models.py
+++ Back-end/api/models.py
@@ -27,42 +27,6 @@ class unique_generator:
         return rideId
 
 # Create your models here.
-# class Driver(models.Model):
-#     driver_id = models.CharField(
-#         max_length=10, default=generate_unique_code_driverid, unique=True)
-#     driver_name = models.CharField(max_length=30, default="", unique=False)
-#     driver_email = models.CharField(max_length=50)
-#     taxi_num = models.CharField(max_length=10, unique=True)
-#     driver_phone = models.CharField(max_length=11)
-#     taxi_test_date = models.DateField(
-#         auto_now=False, auto_now_add=False, max_length=12)
-#     taxi_pollution_validity = models.DateField(
-#         auto_now=False, auto_now_add=False, max_length=12)
-#     taxi_insurance = models.DateField(
-#         auto_now=False, auto_now_add=False, max_length=12)
-#     taxi_type = models.CharField(max_length=10, default='')
-#     taxi_model = models.CharField(max_length=10, default='')
-#     driver_status = models.CharField(max_length=10, default="available")
-#     driver_upi = models.CharField(max_length=10, default='', unique=True)
-#     total_earnings = models.IntegerField(default=0)
-#     total_rides = models.IntegerField(default=0)
-#     total_paid = models.IntegerField(default=0)
-#     total_pending = models.IntegerField(default=0)
-
-
-# class Ride(models.Model):
-#     rideId = models.CharField(max_length=10,default=generate_unique_code, unique=True)
-#     user_name = models.CharField(max_length=250)
-#     start_from = models.CharField(max_length=250)
-#     destination = models.CharField(max_length=250)
-#     requested_time = models.DateTimeField(auto_now_add=True)
-#     starting_time = models.DateTimeField(auto_now_add=False)
-#     reachedtime = models.DateTimeField(auto_now_add=False)
-#     expectedReachingtime = models.TimeField(auto_now_add=False)
-#     status = models.CharField(max_length=20)
-#     carpool = models.BooleanField(default=False)
-#     expectedDriverPay = models.CharField(max_length=7)
-#     carpoolPercent = models.IntegerField(default=0)
 
 
 class NewDriver(models.Model):
@@ -74,14 +38,7 @@ class NewDriver(models.Model):
     driver_dob = models.DateField(auto_now=False, auto_now_add=False, max_length=12)
     driver_status = models.CharField(max_length=20, default="available")
 
-    def save(self, *args, **kwargs):
-            created = not self.pk  # Check if the instance is being created or updated
-
-            super().save(*args, **kwargs)
-
             
-
-
 class TaxiDetail(models.Model):
     driver_id = models.OneToOneField(NewDriver,on_delete=models.DO_NOTHING)
     taxi_num = models.CharField(max_length=10, primary_key=True)
@@ -97,14 +54,11 @@ class NewRideDetail(models.Model):
     rideId = models.IntegerField(max_length=10,default=unique_generator.generate_unique_code_for_rideid, primary_key=True)
     passenger_name = models.CharField(max_length=250)
     driver_id = models.ForeignKey(NewDriver, on_delete=models.DO_NOTHING, default="", null=True)
-    #driver_name = models.OneToOneField(NewDriver,on_delete=models.DO_NOTHING)
-    #driver_name = models.CharField(max_length=100)
     start_from = models.CharField(max_length=250)
     destination = models.CharField(max_length=250)
     requested_time = models.DateTimeField(auto_now_add=True)
     starting_time = models.DateTimeField(auto_now_add=False,null=True)
     reachedtime = models.DateTimeField(auto_now_add=False,null=True)
-    # expectedReachingtime = models.TimeField(auto_now_add=False,null=True)
     status = models.CharField(max_length=20,default="requested")
     carpool = models.BooleanField(default=False)
     expectedDriverPay = models.IntegerField(default=0,max_length=7,null=True)
@@ -121,3 +75,25 @@ class Earning(models.Model):
     total_rides = models.IntegerField(default=int(0))
     total_paid = models.IntegerField(default=int(0))
     total_pending = models.IntegerField(default=int(0))
+
+
+class yellow_tripdata_2016_03(models.Model):
+    VendorID = models.IntegerField()
+    tpep_pickup_datetime = models.DateTimeField()
+    tpep_dropoff_datetime = models.DateTimeField()
+    passenger_count = models.IntegerField()
+    trip_distance = models.FloatField()
+    pickup_longitude = models.FloatField()
+    pickup_latitude = models.FloatField()
+    RatecodeID = models.IntegerField()
+    store_and_fwd_flag = models.CharField(max_length=1)
+    dropoff_longitude = models.FloatField()
+    dropoff_latitude = models.FloatField()
+    payment_type = models.IntegerField()
+    fare_amount = models.FloatField()
+    extra = models.FloatField()
+    mta_tax = models.FloatField()
+    tip_amount = models.FloatField()
+    tolls_amount = models.FloatField()
+    improvement_surcharge = models.FloatField()
+    total_amount = models.FloatField()
