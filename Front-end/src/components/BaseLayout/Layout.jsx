85b5316d9c795e961537a7c3e2731e3e37917632
K K Haridev <kkharidev@gmail.com>
2023-08-20 17:12:31 +0530

    Wrapped base layout within NavbarContext

--- Front-end/src/components/BaseLayout/Layout.jsx
+++ Front-end/src/components/BaseLayout/Layout.jsx
@@ -3,20 +3,21 @@ import { SideBar } from "@components";
 import { Outlet } from "react-router-dom";
 import { Header } from "@components";
 import { Box } from "@chakra-ui/react";
-
+import { NavbarProvider } from "../../context/NavbarContext";
 export const Layout = () => {
   return (
-    <>
+    <NavbarProvider>
       <Header />
       <SideBar />
       <Box
-        marginLeft={["75px","80px"]}
-        padding={["5px","10px"]}
+        marginLeft={{ base: "auto", md: "80px" }}
+        marginRight={{ base: "auto" }}
+        padding={["5px", "10px"]}
         h="calc(100vh - 70px)"
-        w="calc(100vw - 90px)"
+        w={{ base: "90vw", md: "calc(100vw - 90px)" }}
       >
         <Outlet />
       </Box>
-    </>
+    </NavbarProvider>
   );
 };
