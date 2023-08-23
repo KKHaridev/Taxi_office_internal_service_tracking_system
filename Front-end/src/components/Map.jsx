bf30838cd591490613f6b807be85b7cd5704e488
K K Haridev <kkharidev@gmail.com>
2023-08-23 08:07:41 +0530

    Updated map

--- Front-end/src/components/Map.jsx
+++ Front-end/src/components/Map.jsx
@@ -2,19 +2,16 @@ import React from "react";
 import { MapContainer, TileLayer, Marker } from "react-leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 import MapFullScreen from "./MapFullScreen";
-
-export const addressPoints = [
-  [40.780078358949893, -73.960673974467397],
-  [40.646255785919898, -73.784883206389665],
-  [40.770169181619735, -73.872255906701724],
-  [40.725767414067676, -73.996344250052076],
-  [40.755507681362332, -73.982438718280932],
-];
+import { useData } from "../hooks/useData";
 
 export const MapWrapper = ({ height }) => {
+  const { isLoading, error, data } = useData("req_rides", "api/retention");
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
   return (
     <MapContainer
-      center={[addressPoints[1][0], addressPoints[1][1]]}
+      center={[data[1][0], data[1][1]]}
       zoom={10}
       style={{ width: "100%", height: height, zIndex: 10 }}
     >
@@ -23,7 +20,7 @@ export const MapWrapper = ({ height }) => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
       <MarkerClusterGroup>
-        {addressPoints.map((marker, index) => (
+        {data.map((marker, index) => (
           <Marker key={index} position={[marker[0], marker[1]]} />
         ))}
       </MarkerClusterGroup>
